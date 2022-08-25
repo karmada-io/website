6875144b068721d1c9059d6776d5e7f9f9d54e79
Arhell <arhell333@gmail.com>
2022-08-25 04:04:30 +0300

    Update dependencies version
    Signed-off-by: Arhell <arhell333@gmail.com>

--- src/components/featuresList.js
+++ src/components/featuresList.js
@@ -3,7 +3,7 @@ import styles from "../pages/styles.module.css";
 
 function Feature({imgUrl, title, description}) {
     return (
-        <div className="col col--3">
+        <>
             <div className="text--center">
                 {imgUrl && <img className={styles.featureImage} src={useBaseUrl(imgUrl)} alt={title}/>}
             </div>
@@ -11,7 +11,7 @@ function Feature({imgUrl, title, description}) {
                 <h2>{title}</h2>
                 <p>{description}</p>
             </div>
-        </div>
+        </>
     )
 }
 
