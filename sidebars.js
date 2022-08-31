ce423ec4b883d140a0e7000946dae73934aafb7c
RainbowMango <qdurenhongcai@gmail.com>
2022-08-31 11:09:27 +0800

    Add English usecase for AIML INSTITUTE
    Signed-off-by: RainbowMango <qdurenhongcai@gmail.com>

--- sidebars.js
+++ sidebars.js
@@ -199,6 +199,7 @@ module.exports = {
             label: "Case Study",
             items: [
                 "casestudies/vipkid",
+                "casestudies/ci123",
             ],
         },
         {
