a076a50227ec1871d5c56b748a06e36b1a83309c
zhzhuang-zju <m17799853869@163.com>
2024-09-02 16:20:01 +0800

    upgrade from v1.10 to v1.11
    Signed-off-by: zhzhuang-zju <m17799853869@163.com>

--- sidebars.js
+++ sidebars.js
@@ -197,7 +197,8 @@ module.exports = {
                         "administrator/upgrading/v1.6-v1.7",
                         "administrator/upgrading/v1.7-v1.8",
                         "administrator/upgrading/v1.8-v1.9",
-                        "administrator/upgrading/v1.9-v1.10"
+                        "administrator/upgrading/v1.9-v1.10",
+                        "administrator/upgrading/v1.10-v1.11"
                     ],
                 },
                 {
